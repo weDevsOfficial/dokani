adac5edd2cd639c44d7c6777fafdeef6c4a253e9
Sumaiya Siddika <sumaiya.wedevs@gmail.com>
2018-08-10 09:51:35 +0600

    Added new trust factors section template part

---
+++ template-parts/page/section-trust.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Display trust factors content
+ *
+ * @package Dokanee
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+?>
+
+<div class="trust-factors-section">
+    <div class="grid-container">
+        <div class="factor-wrapper">
+            <div class="factor-box">
+                <div class="factor-icon">
+                    <i class="fa fa-check-circle lighten-sky-color"></i>
+                </div>
+                <div class="factor-info">
+                    Fast & Free Delivery
+                </div>
+            </div>
+
+            <div class="factor-box">
+                <div class="factor-icon">
+                    <i class="fa fa-check-circle lighten-sky-color"></i>
+                </div>
+                <div class="factor-info">
+                    Safe & Secure Payment
+                </div>
+            </div>
+
+            <div class="factor-box">
+                <div class="factor-icon">
+                    <i class="fa fa-check-circle lighten-sky-color"></i>
+                </div>
+                <div class="factor-info">
+                    100% Money Back Guaranted
+                </div>
+            </div>
+        </div>
+    </div>
+</div> <!-- .trust-factors-section -->
