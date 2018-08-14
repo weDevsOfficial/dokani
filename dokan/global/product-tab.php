979685b9d4edbaa0e2170a97e313fadc2ae595d1
Sumaiya Siddika <sumaiya.wedevs@gmail.com>
2018-08-14 16:48:31 +0600

    Overwrite dokan product-tab.php for new design

---
+++ dokan/global/product-tab.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Dokan Seller Single product tab Template
+ * 
+ * @package Dokanee
+ */
+?>
+
+<h2><?php _e( 'Vendor Info', 'dokanee' ); ?></h2>
+
+<div class="store-info-wrapper">
+    <div class="store-banner">
+		<?php
+		$banner_id  = isset( $store_info['banner'] ) ? $store_info['banner'] : 0;
+		$banner_url = ( $banner_id ) ? wp_get_attachment_image_src( $banner_id, 'single-vendor-thumb' ) : get_template_directory_uri() . '/assets/images/single-default-store-banner.png';
+
+        if( is_array( $banner_url ) ) {
+        ?>
+            <a href="<?php echo dokan_get_store_url( $seller->ID ); ?>">
+                <img src="<?php echo $banner_url[0]; ?>" alt="<?php echo $store_info['store_name']; ?>">
+            </a>
+            <?php } else { ?>
+            <a href="<?php echo dokan_get_store_url( $seller->ID ); ?>">
+                <img src="<?php echo $banner_url; ?>" alt="<?php echo $store_info['store_name']; ?>">
+            </a>
+        <?php }
+        ?>
+    </div>
+
+    <ul class="store-info">
+		<?php do_action( 'dokan_product_seller_tab_start', $author, $store_info ); ?>
+
+		<?php if ( !empty( $store_info['store_name'] ) ) { ?>
+            <li class="store-name">
+                <?php echo esc_html( $store_info['store_name'] ); ?>
+            </li>
+		<?php } ?>
+
+        <li class="store-rating">
+		    <?php dokan_get_readable_seller_rating( $author->ID ); ?>
+        </li>
+
+        <li class="seller-name">
+            <span class="title"><?php _e( 'Vendor:', 'dokanee' ); ?></span>
+
+            <span class="details">
+                <?php printf( '<a href="%s">%s</a>', dokan_get_store_url( $author->ID ), $author->display_name ); ?>
+            </span>
+        </li>
+		<?php if ( !empty( $store_info['address'] ) ) { ?>
+            <li class="store-address">
+                <span class="title"><?php _e( 'Address:', 'dokanee' ); ?></span>
+                <span class="details">
+                    <?php echo dokan_get_seller_address( $author->ID ) ?>
+                </span>
+            </li>
+		<?php } ?>
+
+		<?php do_action( 'dokan_product_seller_tab_end', $author, $store_info ); ?>
+    </ul>
+</div>
+
