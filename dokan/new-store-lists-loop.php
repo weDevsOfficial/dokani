944eed84271f9094760950d165a9bdc1481d3543
Sumaiya Siddika <sumaiya.wedevs@gmail.com>
2018-08-18 16:34:45 +0600

    Added store list section with all styles and template files

---
+++ dokan/new-store-lists-loop.php
@@ -0,0 +1,62 @@
+<div id="dokan-seller-listing-wrap">
+    <div class="seller-listing-content">
+        <?php if ( $sellers ) : ?>
+            <ul class="dokan-seller-list column-<?php echo $per_row; ?>">
+                <?php
+                foreach ( $sellers as $seller ) {
+                    $store_info = dokan_get_store_info( $seller->get_id() );
+                    $banner_id  = isset( $store_info['banner'] ) ? $store_info['banner'] : 0;
+                    $store_name = isset( $store_info['store_name'] ) ? esc_html( $store_info['store_name'] ) : __( 'N/A', 'dokan-lite' );
+                    $store_url  = dokan_get_store_url( $seller->get_id() );
+                    $store_address  = dokan_get_seller_short_address( $seller->get_id() );
+                    $seller_rating  = dokan_get_seller_rating( $seller->get_id() );
+                    $banner_url = ( $banner_id ) ? wp_get_attachment_image_src( $banner_id, $image_size ) : get_template_directory_uri() . '/assets/images/default-store-banner.png';
+                    $featured_seller = get_user_meta( $seller->get_id(), 'dokan_feature_seller', true );
+                    ?>
+
+                    <li class="dokan-single-seller woocommerce <?php echo ( ! $banner_id ) ? 'no-banner-img' : ''; ?>">
+                        <div class="store-content">
+                            <div class="featured-favourite">
+				                <?php if ( ! empty( $featured_seller ) && 'yes' == $featured_seller ): ?>
+                                    <span class="featured-label"><?php _e( 'Featured', 'dokanee' ); ?></span>
+				                <?php endif ?>
+
+				                <?php do_action( 'dokan_seller_listing_after_featured', $seller, $store_info ); ?>
+                            </div>
+                            <div class="store-banner">
+				                <?php
+				                if( is_array( $banner_url ) ) { ?>
+                                    <a href="<?php echo $store_url; ?>">
+                                        <img src="<?php echo $banner_url[0]; ?>" alt="<?php echo $store_name; ?>">
+                                    </a>
+				                <?php } else { ?>
+                                    <a href="<?php echo $store_url; ?>">
+                                        <img src="<?php echo $banner_url; ?>" alt="<?php echo $store_name; ?>">
+                                    </a>
+				                <?php }
+				                ?>
+                            </div>
+                            <div class="seller-avatar">
+				                <?php echo get_avatar( $seller->get_id(), 55 ); ?>
+                            </div>
+                        </div>
+                        <div class="store-footer">
+                            <div class="store-data">
+                                <h2><a href="<?php echo $store_url; ?>"><?php echo $store_name; ?></a></h2>
+
+				                <?php do_action( 'dokan_seller_listing_after_store_data', $seller, $store_info ); ?>
+
+                            </div>
+
+			                <?php do_action( 'dokan_seller_listing_footer_content', $seller, $store_info ); ?>
+                        </div>
+                    </li>
+
+                <?php } ?>
+                <div class="dokan-clearfix"></div>
+            </ul> <!-- .dokan-seller-wrap -->
+        <?php else:  ?>
+            <p class="dokan-error"><?php _e( 'No vendor found!', 'dokan-lite' ); ?></p>
+        <?php endif; ?>
+    </div>
+</div>
--- template-parts/page/content-front-page.php
+++ template-parts/page/content-front-page.php
@@ -36,3 +36,104 @@ if ( ! defined( 'ABSPATH' ) ) {
         </div>
     </div>
 </section> <!-- .product-cat-section -->
+
+<section class="store-section">
+    <div class="grid-container">
+        <h2 class="section-title"><?php _e( 'Store List', 'dokanee' ); ?></h2>
+
+        <div class="store-wrapper">
+            <div class="tabs">
+                <input type="radio" name="tabs" id="all-store" checked="checked">
+                <label for="all-store">All</label>
+                <div class="tab">
+	                <?php
+	                $new_sellers    = dokan()->vendor->all( array( 'number' => 8 ) );
+
+	                $template_args = array(
+		                'sellers'         => $new_sellers,
+		                'limit'           => $limit,
+		                'offset'          => $offset,
+		                'paged'           => $paged,
+		                'search_query'    => $search_query,
+		                'pagination_base' => $pagination_base,
+		                'per_row'         => 4,
+		                'search_enabled'  => $search,
+		                'image_size'      => $image_size,
+	                );
+
+	                dokan_get_template_part( 'new-store-lists-loop', false, $template_args );
+	                ?>
+                </div>
+
+                <input type="radio" name="tabs" id="best-seller">
+                <label for="best-seller">Best seller</label>
+                <div class="tab">
+	                <?php
+	                $best_sellers = dokan_get_best_sellers(8);
+
+	                $template_args = array(
+		                'sellers'         => $best_sellers,
+		                'limit'           => $limit,
+		                'offset'          => $offset,
+		                'paged'           => $paged,
+		                'search_query'    => $search_query,
+		                'pagination_base' => $pagination_base,
+		                'per_row'         => 4,
+		                'search_enabled'  => $search,
+		                'image_size'      => $image_size,
+	                );
+
+	                ?>
+
+	                <?php dokan_get_template_part( 'best-store-lists-loop', false, $template_args ); ?>
+                </div>
+
+                <input type="radio" name="tabs" id="featured-store">
+                <label for="featured-store">Featured</label>
+                <div class="tab">
+	                <?php
+	                $feature_sellers = dokan_get_feature_sellers(8);
+
+	                $template_args = array(
+		                'sellers'         => $feature_sellers,
+		                'limit'           => $limit,
+		                'offset'          => $offset,
+		                'paged'           => $paged,
+		                'search_query'    => $search_query,
+		                'pagination_base' => $pagination_base,
+		                'per_row'         => 4,
+		                'search_enabled'  => $search,
+		                'image_size'      => $image_size,
+	                );
+
+	                ?>
+
+	                <?php dokan_get_template_part( 'featured-store-lists-loop', false, $template_args ); ?>
+                </div>
+
+                <input type="radio" name="tabs" id="latest-store">
+                <label for="latest-store">New</label>
+
+                <div class="tab">
+                    <?php
+                    $new_sellers    = dokan()->vendor->all( array( 'order' => 'DESC', 'number' => 8 ) );
+
+                    $template_args = array(
+	                    'sellers'         => $new_sellers,
+	                    'limit'           => $limit,
+	                    'offset'          => $offset,
+	                    'paged'           => $paged,
+	                    'search_query'    => $search_query,
+	                    'pagination_base' => $pagination_base,
+	                    'per_row'         => 4,
+	                    'search_enabled'  => $search,
+	                    'image_size'      => $image_size,
+                    );
+
+                    dokan_get_template_part( 'new-store-lists-loop', false, $template_args );
+                    ?>
+                </div>
+            </div>
+        </div>
+    </div>
+</section> <!-- .store-section -->
