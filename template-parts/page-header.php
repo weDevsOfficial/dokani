d570f15847a8c97d8d9c4e98c78accc2e2904682
Sumaiya Siddika <sumaiya.wedevs@gmail.com>
2018-09-10 16:45:05 +0600

    fix: add my account endpoints page title on page header

--- template-parts/page-header.php
+++ template-parts/page-header.php
@@ -40,6 +40,28 @@
                         '<span>' . get_search_query() . '</span>'
                     );
 
+                } elseif ( is_account_page() ) {
+                    global $wp;
+                    $query_vars = $wp->query_vars;
+
+					if ( isset( $query_vars['downloads'] ) ) {
+						_e( 'Downloads', 'dokanee' );
+					} elseif ( isset( $query_vars['orders'] ) ) {
+	                    _e( 'Orders', 'dokanee' );
+					} elseif ( isset( $query_vars['edit-address'] ) ) {
+	                    _e( 'Addresses', 'dokanee' );
+					} elseif ( isset( $query_vars['edit-account'] ) ) {
+	                    _e( 'Account details', 'dokanee' );
+					} elseif ( isset( $query_vars['bookings'] ) ) {
+	                    _e( 'Bookings', 'dokanee' );
+					} elseif ( isset( $query_vars['auctions-endpoint'] ) ) {
+	                    _e( 'Auctions', 'dokanee' );
+					} elseif ( isset( $query_vars['support-tickets'] ) ) {
+	                    _e( 'Support Tickets', 'dokanee' );
+					} else {
+						echo apply_filters( 'dokanee_get_my_account_sub_page_title', __( 'My Account', 'dokanee' ) );
+                    }
+
 				} else {
 					the_title();
 				}
