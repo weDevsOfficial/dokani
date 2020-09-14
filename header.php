98908eba46b4c3899b36bba44556f8e6e0e9f132
mehidi258 <mehidihassan258@gmail.com>
2020-09-14 11:26:24 +0600

    added meta viewport tag

--- header.php
+++ header.php
@@ -14,7 +14,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 <head>
 	<meta charset="<?php bloginfo( 'charset' ); ?>">
 	<meta http-equiv="X-UA-Compatible" content="IE=edge" />
-	<link rel="profile" href="http://gmpg.org/xfn/11">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="profile" href="http://gmpg.org/xfn/11">
 	<?php wp_head(); ?>
 </head>
 
