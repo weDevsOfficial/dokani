0d7e5cbead4ce6e3596514777571d9f7a43a5b47
Xavir Anik <xaviranik.me@gmail.com>
2020-12-21 15:55:57 +0600

    chore: release build process

--- Gruntfile.js
+++ Gruntfile.js
@@ -36,13 +36,91 @@ module.exports = function(grunt) {
                 }
             }
         },
+
+        // Generate POT files.
+        makepot: {
+            target: {
+                options: {
+                    domainPath: '/languages/', // Where to save the POT file.
+                    potFilename: 'dokani.pot', // Name of the POT file.
+                    type: 'wp-theme', // Type of project (wp-plugin or wp-theme).
+                    potHeaders: {
+                        'report-msgid-bugs-to': 'https://wedevs.com/support/forum/theme-support',
+                        'language-team': 'LANGUAGE <EMAIL@ADDRESS>'
+                    }
+                }
+            }
+        },
+
+        // Clean up build directory
+        clean: {
+            main: ['build/']
+        },
+
+        // Copy the theme into the build directory
+        copy: {
+            main: {
+                src: [
+                    '**',
+                    '!node_modules/**',
+                    '!build/**',
+                    '!bin/**',
+                    '!.git/**',
+                    '!Gruntfile.js',
+                    '!package.json',
+                    '!debug.log',
+                    '!phpunit.xml',
+                    '!.gitignore',
+                    '!.gitmodules',
+                    '!npm-debug.log',
+                    '!assets/less/**',
+                    '!tests/**',
+                    '!**/Gruntfile.js',
+                    '!**/package.json',
+                    '!**/README.md',
+                    '!**/export.sh',
+                    '!**/*~'
+                ],
+                dest: 'build/'
+            }
+        },
+
+        //Compress build directory into <name>.zip and <name>-<version>.zip
+        compress: {
+            main: {
+                options: {
+                    mode: 'zip',
+                    archive: './build/_release/dokani-theme-v'+ pkg.version + '.zip'
+                },
+                expand: true,
+                cwd: 'build/',
+                src: ['**/*'],
+                dest: 'dokani'
+            }
+        },
     });
 
     // Load NPM tasks to be used here
+    grunt.loadNpmTasks( 'grunt-wp-i18n' );
+    grunt.loadNpmTasks( 'grunt-contrib-clean' );
+    grunt.loadNpmTasks( 'grunt-contrib-copy' );
+    grunt.loadNpmTasks( 'grunt-contrib-compress' );
     grunt.loadNpmTasks( 'grunt-contrib-less' );
     grunt.loadNpmTasks( 'grunt-contrib-watch' );
 
     grunt.registerTask( 'default', [
         'watch',
+    ]);
+
+
+    grunt.registerTask('release', [
+        'makepot',
+        'less'
+    ]);
+
+    grunt.registerTask( 'zip', [
+        'clean',
+        'copy',
+        'compress'
     ]);
 };
