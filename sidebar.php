2f56804ed67062373fac3c5078d5f9198153781d
Ahsan Habib Shaheen <ahsanshaheen199@gmail.com>
2022-05-24 22:54:01 +0600

    Fixed store page default widget warning issue

--- sidebar.php
+++ sidebar.php
@@ -112,16 +112,16 @@ if ( function_exists( 'dokani_secondary_nav_get_defaults' ) ) {
                         'after_title'   => '</h3>',
                     );
 
-                    if ( class_exists( 'store_location' ) ) :
+                    if ( class_exists( 'Dokan_Store_Location' ) ) :
 
-                        the_widget( 'store_category_menu', array( 'title' => __( 'Store Category', 'dokani' ) ), $args );
+                        the_widget( 'WeDevs\Dokan\Widgets\StoreCategoryMenu', array( 'title' => __( 'Store Category', 'dokani' ) ), $args );
 
-                        if ( dokan_get_option( 'store_map', 'dokan_general', 'on' ) == 'on' && ! empty( $map_location ) ) {
-                            the_widget( 'store_location', array( 'title' => __( 'Store Location', 'dokani' ) ), $args );
+                        if ( dokan_get_option( 'store_map', 'dokan_general', 'on' ) == 'on' ) {
+                            the_widget( 'WeDevs\Dokan\Widgets\StoreLocation', array( 'title' => __( 'Store Location', 'dokani' ) ), $args );
                         }
 
                         if ( dokan_get_option( 'contact_seller', 'dokan_general', 'on' ) == 'on' ) {
-                            the_widget( 'store_contact_form', array( 'title' => __( 'Contact Vendor', 'dokani' ) ), $args );
+                            the_widget( 'WeDevs\Dokan\Widgets\StoreContactForm', array( 'title' => __( 'Contact Vendor', 'dokani' ) ), $args );
                         }
                     endif;
 
@@ -143,14 +143,14 @@ if ( function_exists( 'dokani_secondary_nav_get_defaults' ) ) {
                     <?php
 
                     if ( class_exists( 'Dokan_Store_Location' ) ) {
-                        the_widget( 'Dokan_Store_Category_Menu', array( 'title' => __( 'Store Category', 'dokani' ) ), $args );
+                        the_widget( 'WeDevs\Dokan\Widgets\StoreCategoryMenu', array( 'title' => __( 'Store Category', 'dokani' ) ), $args );
 
-                        if ( dokan_get_option( 'store_map', 'dokan_general', 'on' ) == 'on' && ! empty( $map_location ) ) {
-                            the_widget( 'Dokan_Store_Location', array( 'title' => __( 'Store Location', 'dokani' ) ), $args );
+                        if ( dokan_get_option( 'store_map', 'dokan_general', 'on' ) == 'on' ) {
+                            the_widget( 'WeDevs\Dokan\Widgets\StoreLocation', array( 'title' => __( 'Store Location', 'dokani' ) ), $args );
                         }
 
                         if ( dokan_get_option( 'contact_seller', 'dokan_general', 'on' ) == 'on' ) {
-                            the_widget( 'Dokan_Store_Contact_Form', array( 'title' => __( 'Contact Vendor', 'dokani' ) ), $args );
+                            the_widget( 'WeDevs\Dokan\Widgets\StoreContactForm', array( 'title' => __( 'Contact Vendor', 'dokani' ) ), $args );
                         }
                     }
 
